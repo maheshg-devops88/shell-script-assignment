07aa934d72afd297fbbd52e218832129fa03bb17
mahesh <mahesh050917@gmail.com>
2026-08-10 15:51:51 +0530

    added shell scripts

--- 01.fileexist.sh
+++ 01.fileexist.sh
@@ -2,6 +2,7 @@
 
 user_id=$(id -u)
 
+
 if [ $user_id -ne 0 ]; then
       echo "Please run the Script with sudo access" 
       exit 1
@@ -10,12 +11,28 @@ fi
 if [ $# -eq 0 ]; then
    echo "Please provide the file name or directory to check if exists"
    exit 1
+  else 
+    for Name in $@ ; then
+        if [ -f $NAME ]; 
+           echo "Please find the file name provided: $NAME"
+           echo "Checking if file exists.....$NAME"
+           $filetype=$(find / -type f -name $NAME)
+
+           if [ $? -eq 0 ];
+              echo "please find the file $NAME exists in path $filetype"
+           fi
+       else
+             echo "File name $NAME does not exists"
+        fi
+    done
 fi
 
+        
+            #    if [ -d $NAME ];
+            #       $filetype=$(find / -type d -name $NAME)
+            #       echo "Please find the Dir $Name exists in path $filetype 
+            #       cd $filetype
+            #       echo "Please find the list of files in dir $filetype: $(ls -ltr $filetype)
+            #    fi
 
-if [ -f $1 ]; then
-    echo "File name $1 exists"
-  else
-    echo "File name does not exists"
-fi
     
