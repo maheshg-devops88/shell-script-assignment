6a8c3d274f547d11bd0f2f445763e635428ad16c
mahesh <mahesh050917@gmail.com>
2026-08-10 15:11:25 +0530

    added shell scripts

--- 01.fileexist.sh
+++ 01.fileexist.sh
@@ -5,3 +5,10 @@ user_id=$(id -u)
 if [ $user_id -ne 0 ]; then
       echo "Please run the Script with sudo access" 
 fi
+
+if [ -f $1 ]; then
+    echo "File name $1 exists"
+  else
+    echo "File name does not exists"
+fi
+    
