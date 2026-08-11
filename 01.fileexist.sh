19a24c28b144ca71aba7a2bb33b1173e33ce6c71
mahesh <mahesh9.storage@gmail.com>
2026-08-11 11:00:28 +0530

    added shell scripts

--- 01.fileexist.sh
+++ 01.fileexist.sh
@@ -15,10 +15,11 @@ fi
     
 for NAME in $@ ; do
         filetype=$(find / -type f -name $NAME)
-        if [ -n $filetype ]; then
-              echo "File name $NAME does not exists"
-      else
+        if [ -n "$filetype" ]; then
+             
               echo "please find the file $NAME exists in path $filetype"
+      else
+              echo "File name $NAME does not exists"
         fi
     done
 
