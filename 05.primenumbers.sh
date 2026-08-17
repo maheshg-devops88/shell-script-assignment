d5c722aa0ed2d7253b98ae34122dc3a887fa10f1
mahesh <mahesh9.storage@gmail.com>
2026-08-17 13:00:34 +0530

    added scripts

---
+++ 05.primenumbers.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+user_id=$(id -u)
+
+
+if [ $user_id -ne 0 ]; then
+      echo "Please run the Script with sudo access" 
+      exit 1
+fi
+
+if [ $# -eq 0 ]; then
+   echo "Please provide the number to check if it prime or not"
+   exit 1
+fi
+
+for NUM in $@; then
+   
+     if [ $NUM -lt 2 ]; then
+        echo "$NUM is not prime"
+        continue
+     fi
+ 
+     IS_PRIME=1
+
+   for ((i=2,i*i<=NUM;i++)); do
+       
+       if (( NUM % i == 0 )); then 
+           IS_PRIME=0
+           break
+       fi
+   done
+
+    if [ "$IS_PRIME" -eq 0 ]; then
+       
+       echo "$NUM is prime" 
+      else
+       echo "$NUM is not prime"
+    fi
+
+done
+
