888af38d3d335acc6a83bd8f5cab0335607085c8
mahesh <mahesh9.storage@gmail.com>
2026-08-18 20:16:04 +0530

    shell script added

---
+++ 08.factorial.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+user_id=$(id -u)
+
+
+if [ $user_id -ne 0 ]; then
+      echo "Please run the Script with sudo access" 
+      exit 1
+fi
+
+if [ $# -eq 0 ]; then
+   echo "Please provide the number to provide the factorial number"
+fi
+
+n=$1
+fact=1
+
+for $((i=1; i<=$n ; i++)); do
+
+   fact=$(($fact * i))
+
+done
+
+echo "factorial number for $n is $fact"
