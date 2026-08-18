5d7a61b274f740c657661c23248844aa1e343ae2
mahesh <mahesh9.storage@gmail.com>
2026-08-18 19:30:59 +0530

    shell script added

--- 06.palindrome.sh
+++ 06.palindrome.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 user_id=$(id -u)
 
 
---
+++ 07.Fibonacci.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+user_id=$(id -u)
+
+
+if [ $user_id -ne 0 ]; then
+      echo "Please run the Script with sudo access" 
+      exit 1
+fi
+
+if [ $# -eq 0 ]; then
+   echo "Please provide the number to calculate Fibonacci Series"
+   exit 1
+fi
+
+a=0
+b=1
+
+for (i=0; i<$n; i++); do 
+    echo -n "$a "
+    c=$((a+b))
+
+    a=$b
+    b=$c
+done 
+
