4f899c59782cd8947268882a15a2e8e96117c652
mahesh <mahesh9.storage@gmail.com>
2026-08-20 09:13:46 +0530

    added scripts

---
+++ 10.FilterError.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+user_id=$(id -u)
+
+
+if [ $user_id -ne 0 ]; then
+      echo "Please run the Script with sudo access" 
+      exit 1
+fi
+
+if [ $# -eq 0 ]; then
+   echo "Please provide the Input file to check the errors"
+   exit 1
+fi
+
+INPUT_FILE=$1
+OUTPUT_FILE="error_lines.csv"
+
+{
+      echo "Saving ERROR to new File"
+      grep "stopped" "$INPUT_FILE"
+
+} > "$OUTPUT_FILE"
+
+echo "Filtered error saved to $OUTPUT_FILE"
